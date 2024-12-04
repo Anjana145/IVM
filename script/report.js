c05013ebe8d6620a2fb419b19cf86ceede02a786
Anjana145 <zanjana150@gmail,com>
2024-12-04 17:13:48 +0545

    report where u can review the report of items and all (report.html)

---
+++ script/report.js
@@ -0,0 +1,6 @@
+<script>
+// Set the report date to the current date
+document.getElementById('report-date').innerText = new Date().toLocaleDateString();
+
+// Additional JavaScript can be added here (e.g., dynamic calculations or exporting functionality)
+</script>
